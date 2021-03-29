129417ce4b5b34a990f5ba22c7b08c146ffd7cdf
Fafofafoy <ihcaoe@gmail.com>
2021-03-29 09:07:17 +0800

    整理symbols.py

--- tshamsoo/symbols.py
+++ tshamsoo/symbols.py
@@ -4,8 +4,6 @@
 Defines the set of symbols used in text input to the model.
 
 The default is a set of ASCII characters that works well for English or text that has been run through Unidecode. For other data, you can modify _characters. See TRAINING_DATA.md for details. '''
-from utils.text import cmudict
-
 _pad = '_'
 _punctuation = '!\'(),.:;? '
 _special = '-'
